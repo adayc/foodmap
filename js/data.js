efe83534c1ecfa81baf7aa8c5fec33af7ca8c7ec
adayc <adayc@hotmail.com>
2017-12-31 04:12:58 -0500

    Creando data.js

---
+++ js/data.js
@@ -0,0 +1,18 @@
+var restaurants = [
+  {	name:	'DON ROSALINO',
+    addres: 'Av. Aviación 2667, San Borja 15037',
+    district:	'MIRAFLORES',
+    phone:	'01-4476805',
+    mail:	'miraflores@donrosalino.com',
+    website:	'www.donrosalino.com',
+    horario:	'8:00-15:00',
+    petfriendly:	true,
+    delivery:	true,
+    rating:	'5',
+    image:	'don-rosalino.jpg',
+    iframe:	'<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3901.2742554645574!2d-77.00480768585558!3d-12.093367746060187!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c7d0ef8b8fad%3A0x1add09e9a34164db!2sDon+Rosalino+Ristorante!5e0!3m2!1ses-419!2spe!4v1514709517882" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>',
+    kind:	'Cocina Italiana'	
+  }	
+
+
+];
